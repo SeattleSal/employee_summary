4bcf79211c605ad3631bef3e92cb64e10e15ae8c
Sally Perez <seattlesally1@gmail.com>
2020-12-11 15:45:20 -0700

    added in app.js functions and prompts for manager, engineer and intern info

--- app.js
+++ app.js
@@ -10,9 +10,185 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employeeList = [];
+// const idArray = [];
+
+// function for ask user for manager info
+function askUserforManagerInfo() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: `Enter manager's name.`,
+            name: `mgrName`,
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            type: 'input',
+            message: "Enter manager's ID",
+            name: "mgrId"
+        },
+        {
+            type: 'input',
+            message: `Enter manager email.`,
+            name: `mgrEmail`
+        },
+        {
+            type: 'input',
+            message: "Enter manager office number.",
+            name: "officeNum"
+        }
+    ]).then((response) => {
+        //create manager
+        const newManager = new Manager(response.mgrName, response.mgrId, response.mgrEmail, response.officeNum);
+        employeeList.push(newManager);
+        askForEmployeeType();
+    }); 
+}
+
+// function for ask for next employee type
+function askForEmployeeType(){
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "nextEmployee",
+            message: "Choose next employee type",
+            choices: ["Engineer", "Intern", "No more employees"]
+
+        }
+    ]).then((results) =>{
+        console.log(results);
+        switch (results.nextEmployee) {
+            case "Engineer":
+            console.log("You chose engineer!");
+            askUserForEngineerInfo();
+            break;
+
+            case "Intern":
+            console.log("you chose intern!")
+            askUserForInternInfo();
+            break;
+
+            case "No more employees":
+            console.log("No more employees done.");
+            console.log(employeeList);
+            // print html page with employee list
+
+        }
+    })
+
+}
+
+// function for ask user for engineer info
+function askUserForEngineerInfo(){
+    return inquirer.prompt([
+        {
+            name: `engineerName`,
+            type: 'input',
+            message: `Enter Engineer's name.`,
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            name: "engineerId",
+            type: 'input',
+            message: "Enter Engineer's ID"
+        },
+        {
+            name: `engineerEmail`,
+            type: 'input',
+            message: `Enter Engineer's email.`
+        },
+        {
+            name: "github",
+            type: 'input',
+            message: "Enter Engineer's github."
+        }
+    ]).then((response) => {
+        //create Engineer
+        const newEngineer = new Engineer(response.engineerName, response.engineerId, response.engineerEmail, response.github);
+        employeeList.push(newEngineer);
+        askForEmployeeType();
+    }); 
+
+}
+
+// function for ask user for intern info
+function askUserForInternInfo(){
+    return inquirer.prompt([
+        {
+            name: `internName`,
+            type: 'input',
+            message: `Enter Intern's name.`,
+            validate: answer => {
+                if (answer !== "") {
+                    return true;
+                }
+                return "Please enter at least one character.";
+            }
+        },
+        {
+            name: "internId",
+            type: 'input',
+            message: "Enter Intern's ID"
+        },
+        {
+            name: `internEmail`,
+            type: 'input',
+            message: `Enter Intern's email.`
+        },
+        {
+            name: "school",
+            type: 'input',
+            message: "Enter Intern's School."
+        }
+    ]).then((response) => {
+        //create Intern
+        const newIntern = new Intern(response.internName, response.internId, response.internEmail, response.school);
+        employeeList.push(newIntern);
+        askForEmployeeType();
+    }); 
+}
+
+function appMenu() {
+    function createManager() {
+        console.log("Please build your team");
+        inquirer.prompt(managerQuestions)
+        //
+    }
+}
+
+// ask user for manager info
+
+// prompt user for next employee type (engineer, intern) or no more employees
+
+// ask user for engineer info
+
+// ask user for intern info
+
+
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function init(){
+    askUserforManagerInfo();
+    // let empList = [];
+    // let emp1 = new Manager("Sal", 1234, "sal@mail.com", 4444);
+    // empList.push(emp1);
+    // console.log(empList);
+    // render(empList);
+
+}
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +209,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+init();
